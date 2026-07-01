f5e291124d9db7865b9244aea2f6dbb300ec7681
Dhanusha <dhanushapatil1623@gmail.com>
2026-07-01 18:58:36 +0530

    Added Python Sets Basics Program

---
+++ sets_basics.py
@@ -0,0 +1,15 @@
+# Program: Python Sets Basics
+# Author: Dhanusha
+# Topic: Introduction to Sets
+
+# Creating a Set
+fruits = {"Apple", "Mango", "Banana", "Apple", "Mango"}
+
+print("Original Set:")
+print(fruits)
+
+print("\nNumber of Unique Fruits:")
+print(len(fruits))
+
+print("\nExplanation:")
+print("Sets automatically remove duplicate values.")
